bca23b19e854f9f88777d86a68d84935b5e6d152
ophanim-crimson <asherjohnjobin2028@cs.ajce.in>
2026-06-24 21:54:39 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Report from './pages/Report';
-import ReportDetail from './pages/ReportDetail';
+import Home from '../pages/Home';
+import Report from '../pages/Report';
+import ReportDetail from '../pages/ReportDetail';
+
 
 const App: React.FC = () => {
   return (
